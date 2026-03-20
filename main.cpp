bb9b8b0f344c0141cd04ad92269c54b50b9ab530
pointer colt <codealt116@gmail.com>
2026-03-20 16:46:56 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std ;
+
+
+int main(){
+    
+    cout << "Try and guess my animal" << endl ;
+
+    string animal = "Elephant" ;
+    string question ;
+
+    for ( int i{} ; i < 10 ; ++i){
+        cout << "Enter a question : " ;
+        getline(cin,question);
+        
+
+    }
+
+
+
+
+
+
+    return 0;
+}
